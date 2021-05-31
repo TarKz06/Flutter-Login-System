8326961e873ac413425fb1132d97b1f0a36fb9ea
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-31 13:31:17 +0700

    Home Screen

---
+++ lib/screen/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        title: Text("Register/Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text(
+                    "Register",
+                    style: TextStyle(fontSize: 20),
+                  )),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.login),
+                  label: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+    return scaffold;
+  }
+}
