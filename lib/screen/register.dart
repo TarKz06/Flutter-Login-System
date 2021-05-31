438b86f1e51016643615aee47fd67267b155f81b
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-31 15:33:30 +0700

    Keyform

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -6,6 +6,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +17,23 @@ class _RegisterScreenState extends State<RegisterScreen> {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Form(
+            key: formkey,
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("Email", style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                  ),
                   TextField(),
                   SizedBox(
                     height: 15,
                   ),
                   Text("Password", style: TextStyle(fontSize: 20)),
-                  TextFormField(),
+                  TextFormField(
+                    obscureText: true,
+                  ),
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
