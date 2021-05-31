a0619e3ba2ff0da6f0a80c7010092fb9d37542a5
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-31 14:41:52 +0700

    Navigation

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/login.dart';
+import 'package:flutter_application_1/screen/register.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -10,13 +12,18 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
         child: SingleChildScrollView(
-                  child: Column(
+          child: Column(
             children: [
               Image.asset("assets/images/schoolgirl.png"),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton.icon(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return RegisterScreen();
+                      }));
+                    },
                     icon: Icon(Icons.add),
                     label: Text(
                       "Register",
@@ -26,7 +33,12 @@ class HomeScreen extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton.icon(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return LoginScreen();
+                      }));
+                    },
                     icon: Icon(Icons.login),
                     label: Text(
                       "Login",
