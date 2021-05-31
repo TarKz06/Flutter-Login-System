0e379a0726262637037720e12f65249837694f57
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-31 14:57:41 +0700

    Form

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -8,9 +8,40 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Email", style: TextStyle(fontSize: 20)),
+                  TextField(),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("Password", style: TextStyle(fontSize: 20)),
+                  TextFormField(),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
