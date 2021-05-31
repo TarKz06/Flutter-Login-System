077cf1e04c85fa316bc6208de618047d4ca9933a
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-31 16:33:59 +0700

    Firebase Bluider

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/model/profile.dart';
 import 'package:form_field_validator/form_field_validator.dart';
@@ -10,69 +11,94 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final formkey = GlobalKey<FormState>();
   Profile profile = Profile();
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Form(
-            key: formkey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Email", style: TextStyle(fontSize: 20)),
-                  TextFormField(
-                    validator: MultiValidator([
-                      RequiredValidator(errorText: "pls fill your email"),
-                      EmailValidator(errorText: "Your email not correct")
-                    ]),
-                    keyboardType: TextInputType.emailAddress,
-                    onSaved: (String email) {
-                      profile.email = email;
-                    },
-                  ),
-                  TextField(),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text("Password", style: TextStyle(fontSize: 20)),
-                  TextFormField(
-                    validator: RequiredValidator(
-                        errorText: ("Pls fill your Password")),
-                    obscureText: true,
-                    onSaved: (String password) {
-                      profile.password = password;
-                    },
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      child: Text(
-                        "Register",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      onPressed: () {
-                        if (formkey.currentState.validate()) {
-                          formkey.currentState.save();
-                          print(
-                              "email = ${profile.email} password = ${profile.password}");
-                          formkey.currentState.reset();
-                        }
-                      },
-                    ),
-                  )
-                ],
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Error"),
               ),
+              body: Center(
+                child: Text("${snapshot.error}"),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Register"),
+              ),
+              body: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Form(
+                    key: formkey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Email", style: TextStyle(fontSize: 20)),
+                          TextFormField(
+                            validator: MultiValidator([
+                              RequiredValidator(
+                                  errorText: "pls fill your email"),
+                              EmailValidator(
+                                  errorText: "Your email not correct")
+                            ]),
+                            keyboardType: TextInputType.emailAddress,
+                            onSaved: (String email) {
+                              profile.email = email;
+                            },
+                          ),
+                          TextField(),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text("Password", style: TextStyle(fontSize: 20)),
+                          TextFormField(
+                            validator: RequiredValidator(
+                                errorText: ("Pls fill your Password")),
+                            obscureText: true,
+                            onSaved: (String password) {
+                              profile.password = password;
+                            },
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              child: Text(
+                                "Register",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              onPressed: () {
+                                if (formkey.currentState.validate()) {
+                                  formkey.currentState.save();
+                                  print(
+                                      "email = ${profile.email} password = ${profile.password}");
+                                  formkey.currentState.reset();
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
+    //
   }
 }
